ce4570ce0468045b48d235d0431c782297fd3201
yuliapchr <ulapechurenko@gmail.com>
2025-03-10 15:42:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum'
 
 class LoginForm(FlaskForm):
-    astronaut_id = StringField('ID астронавта', validators=[DataRequired()])
+    astronaut_id = StringField('id астронавта', validators=[DataRequired()])
     astronaut_password = PasswordField('Пароль астронавта', validators=[DataRequired()])
-    capitan_id = StringField('ID капитана', validators=[DataRequired()])
+    capitan_id = StringField('id капитана', validators=[DataRequired()])
     capitan_password = PasswordField('Пароль капитана', validators=[DataRequired()])
     access = SubmitField('Доступ')
 
