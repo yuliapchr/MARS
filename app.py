9a5d9bc83fac3dcf8c220aebf96754f5a1a13928
yuliapchr <ulapechurenko@gmail.com>
2025-03-24 17:04:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
-from flask import Flask, render_template, request
+import datetime
+from flask import Flask, render_template, request, make_response, session
 from flask_wtf import FlaskForm
 from werkzeug.utils import redirect
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.fields.simple import EmailField, BooleanField
 from wtforms.validators import DataRequired
+from data.users import User
+from data import db_session
+from data.jobs import Jobs
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum'
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(days=365)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 
 class LoginForm(FlaskForm):
-    astronaut_id = StringField('id астронавта', validators=[DataRequired()])
-    astronaut_password = PasswordField('Пароль астронавта', validators=[DataRequired()])
-    capitan_id = StringField('id капитана', validators=[DataRequired()])
-    capitan_password = PasswordField('Пароль капитана', validators=[DataRequired()])
-    access = SubmitField('Доступ')
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class JobsForm(FlaskForm):
+    title = StringField("Название работы")
+    team_leader = StringField('ID тим лидера')
+    work_size = StringField('Продолжительность')
+    collaborators = StringField('Участники')
+    finished = BooleanField('Завершена?')
+    submit = SubmitField('Добавить')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route('/')
@@ -77,7 +102,8 @@ def training(prof):
 def list_prof(lst):
     context = {
         'list': lst,
-        'profs': ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'инженер по терраформированию', 'климатолог']
+        'profs': ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'инженер по терраформированию',
+                  'климатолог']
     }
     return render_template('list_prof.html', **context)
 
@@ -85,11 +111,67 @@ def list_prof(lst):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if request.method == 'GET':
-        return render_template('login.html', form=form)
     if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/cookie')
+def cookie():
+    visits_count = int(request.cookies.get("visits_count", 0))
+    if visits_count:
+        res = make_response(
+            f"Вы пришли на эту страницу {visits_count + 1} раз")
+        res.set_cookie("visits_count", str(visits_count + 1),
+                       max_age=60 * 60 * 24 * 365 * 2)
+    else:
+        res = make_response(
+            "Вы пришли на эту страницу в первый раз за последние 2 года")
+        res.set_cookie("visits_count", '1',
+                       max_age=60 * 60 * 24 * 365 * 2)
+    return res
+
+
+@app.route("/session_test")
+def session_test():
+    visits_count = session.get('visits_count', 0)
+    session['visits_count'] = visits_count + 1
+    return make_response(
+        f"Вы пришли на эту страницу {visits_count + 1} раз")
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/jobs', methods=['GET', 'POST'])
+def jobs():
+    form = JobsForm()
+    if form.validate_on_submit():
+        sess = db_session.create_session()
+        job = Jobs()
+        job.job = form.title.data
+        job.team_leader = form.team_leader.data
+        job.work_size = form.work_size.data
+        job.collaborators = form.collaborators.data
+        job.is_finished = form.finished.data
+        current_user.jobs.append(job)
+        sess.merge(current_user)
+        sess.commit()
         return redirect('/')
+    return render_template('jobs.html', form=form)
 
 
 if __name__ == '__main__':
+    db_session.global_init('database/mars_explorer.db')
     app.run(host='127.0.0.1', port=8080)
