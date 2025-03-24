c0b7b1d793b1db85f77d966914a9fadbcfeac702
yuliapchr <ulapechurenko@gmail.com>
2025-03-24 18:39:54 +0300

    Update jobs_api.py

--- jobs_api.py
+++ jobs_api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, make_response
 from data import db_session
 from data.jobs import Jobs
 
@@ -10,5 +10,18 @@ def get_jobs():
     sess = db_session.create_session()
     jobs_list = sess.query(Jobs).all()
     return jsonify({
-        'jobs': [job.to_dict(only=('id', 'user.name', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished')) for job in jobs_list]
+        'jobs': [job.to_dict(
+            only=('id', 'user.name', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished')) for
+            job in jobs_list]
+    })
+
+
+@blueprint.route('/api/jobs/<int:job_id>')
+def get_jobs_id(job_id):
+    sess = db_session.create_session()
+    jobs_id = sess.query(Jobs).filter(Jobs.id == job_id).first()
+    if not jobs_id:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify({
+        'jobs_id': jobs_id.to_dict()
     })
